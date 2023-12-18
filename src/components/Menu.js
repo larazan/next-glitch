c1df06fe8554015163e5820408837f170d2038fe
zamroni <forheron@gmail.com>
2023-12-18 12:30:15 +0700

    fix menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -49,23 +49,23 @@ export default function Menu() {
         <button
           ref={trigger}
           // className={`w-8 h-8 flex items-center justify-center bg-slate-100 hover:bg-slate-200 transition duration-150 rounded-full ${dropdownOpen && 'bg-slate-200'}`}
-          className="flex  items-center shadow-menu justify-center rounded border-2 border-gray-800 px-1.5 md:px-3 py-1.5 space-x-1 text-gray-900 bg-white opacity-90 hover:opacity-100"
+          className="flex  items-center shadow-menu justify-center rounded border-2 border-gray-800 px-1.5 md:px-3 py-1.5 md:space-x-1 text-gray-900 bg-white opacity-90 hover:opacity-100"
           aria-haspopup="true"
           onClick={() => setDropdownOpen(!dropdownOpen)}
           aria-expanded={dropdownOpen}
         >
             <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </span>
-          <span className="text-xs text-gray-900 font-semibold font-mabrybold">Menu</span>
+          <span className="hidden md:block text-xs text-gray-900 font-semibold font-mabrybold">Menu</span>
          
         </button>
         
   
         <Transition
-          className="origin-top-right shadow-menu  absolute top-full right-0 -mr-2 md:-mr-16 sm:mr-0 min-w-80 w-64 bg-white border-2 border-gray-600 py-1.5 rounded shadow-lg overflow-hidden mt-4 md:mt-1"
+          className="origin-top-right shadow-menu  absolute top-full right-0 -mr-2 md:-mr-16 sm:mr-0 min-w-80 w-64 bg-white border-2 border-gray-600 py-1.5 rounded shadow-lg overflow-hidden mt-2 md:mt-1"
           show={dropdownOpen}
           enter="transition ease-out duration-200 transform"
           enterStart="opacity-0 -translate-y-2"
